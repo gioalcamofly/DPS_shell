c2289e1cf37508b6bec09b01fdf7c52a5d6aac2c
gioalcamofly <giovanni.alberto@hotmail.com>
2018-03-25 20:30:13 +0100

    Final Version

--- shell.c
+++ shell.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define SIZE 2048
 #define MAX_ARGS 20
+#define MAX_CMDS 20
 
 struct command {
 	char *name;
@@ -32,8 +34,11 @@ int built_in(struct command *cmd) {
 	} else if (strcmp(cmd->name, "cd") == 0) {
 		chdir(cmd->argv[1]);
 		return 1;
+	} else if (strcmp(cmd->name, "ll") == 0) {
+		cmd->name = "ls";
+		cmd->argv[0] = "ls";
+		cmd->argv[1] = "-l";
 	}
-
 	return 0;
 }
 
@@ -67,7 +72,8 @@ char *read_input() {
 
 		if (c == '&') {
 			background = 1;
-			continue;
+			input[i]='\0';
+			return input;
 		}
 
 		if (i >= buffsize) {
@@ -126,17 +132,15 @@ struct command *get_cmd (char *input) {
 		}
 
 		cmd->argv[i++] = token;
-
+		
 		if (i > MAX_ARGS) {
 			fprintf(stderr, "Error! The maximum number of arguments is %d\n", MAX_ARGS);
-			exit(1);
+			return NULL;
 		}
 		token = strtok(NULL, " ");
 	}
 
 	cmd->name = cmd->argv[0];
-	cmd->argc = i;
-
 	return cmd;
 
 }
@@ -150,6 +154,14 @@ struct pipeline *get_cmd_with_pipes (char *input) {
 	single_cmd = strtok_r(input, "|", &ptr);
 	while (single_cmd != NULL) {
 		pipeline->cmd[i++] = get_cmd(single_cmd);
+
+		if (pipeline->cmd[i-1] == NULL) {
+			return NULL;
+		}
+		if (i > MAX_CMDS) {
+			fprintf(stderr, "Error! The maximum number of commands is %d\n", MAX_CMDS);
+			return NULL;
+		}
 		single_cmd = strtok_r(NULL, "|", &ptr);
 	}
 
@@ -160,57 +172,73 @@ struct pipeline *get_cmd_with_pipes (char *input) {
 
 void close_pipes(int (*pipes)[2]) {
 	int i;
-
+	
 	for (i = 0; i < pipe_num; i++) {
 		close(pipes[i][0]);
 		close(pipes[i][1]);
 	}
 }
 
-int exec_cmd (struct command *cmd, int (*pipes)[2]) {
+void clean (struct pipeline *pipeline) {
+	int i;
+	for (i = 0; i < pipeline->count; i++) {
+		free(pipeline->cmd[i]);
+	}
+	free(pipeline);
+}
+
+int exec_cmd (struct command *cmd, int (*pipes)[2], struct pipeline *pipeline) {
 	pid = fork();
 	if (pid == 0) {
-
+		
 		int input_fd = cmd->fd[0];
 		int output_fd = cmd->fd[1];
-
+		
 		if (input_fd != -1 && input_fd != STDIN_FILENO) {
 			dup2(input_fd, STDIN_FILENO);
 		}
-
+		
 		if (output_fd != -1 && output_fd != STDOUT_FILENO) {
 			dup2(output_fd, STDOUT_FILENO);
 		}
-
+		
 		if (pipes != NULL) {
 			close_pipes(pipes);
 		}
-
+		
 		execvp(cmd->name, cmd->argv);
 
+	
+		//Solo en caso de error en el exec
+		fprintf(stderr, "[Error] %s\n", strerror(errno));
+
+		free(pipes);
+		free(input);
+		clean(pipeline);
+
 	}
-	return 0;
+	return pid;
 }
 
 
-void exec_pipe (struct pipeline *pipeline) {
+int exec_pipe (struct pipeline *pipeline) {
+	int res;
 	int i = pipeline->count;
 	if (i == 1) {
 		pipeline->cmd[0]->fd[0]=STDIN_FILENO;
 		pipeline->cmd[0]->fd[1]=STDOUT_FILENO;
-		exec_cmd(pipeline->cmd[0], NULL);
+		exec_cmd(pipeline->cmd[0], NULL, pipeline);
 		if (background == 1) {
 			waitpid(pid, &status, WNOHANG);
-			background = 0;
 		} else {
 			waitpid(pid, &status, NULL);
 		}
-
+		
 	} else {
 		pipe_num = i - 1;
 		int (*pipes)[2] = calloc(pipe_num * sizeof(int[2]), 1);
 		int j;
-
+		
 		pipeline->cmd[0]->fd[0] = STDIN_FILENO;
 		for (j = 1; j < i; j++) {
 			pipe(pipes[j-1]);
@@ -218,18 +246,24 @@ void exec_pipe (struct pipeline *pipeline) {
 			pipeline->cmd[j]->fd[0] = pipes[j-1][0];
 		}
 		pipeline->cmd[pipe_num]->fd[1] = STDOUT_FILENO;
-
+		
 		for (j = 0; j < i; j++) {
-			exec_cmd(pipeline->cmd[j], pipes);
+			res = exec_cmd(pipeline->cmd[j], pipes, pipeline);
 		}
-
+		
 		close_pipes(pipes);
-
-		for (j = 0; j < i; ++j) {
-			waitpid(pid, &status, NULL);
+		
+		for (j = 0; j < i; j++) {
+			if (background == 1) {
+				waitpid(-1, &status, WNOHANG);
+			} else {
+				wait(NULL);
+			}		
 		}
+		free(pipes);
 	}
-
+	return res;
+	
 }
 
 
@@ -246,25 +280,34 @@ int main() {
 		printf("[%s@%s %s]#", getenv("USERNAME"), getenv("HOSTNAME"), dir);
 
 		input = read_input();
-		struct pipeline *pipeline = get_cmd_with_pipes(input);
 
 		if (input == NULL) {
 			printf("finalización del programa");
 			exit(0);
+		}
+
+		struct pipeline *pipeline = get_cmd_with_pipes(input);
+
+		if (pipeline == NULL) {
+			continue;
 		} else if ((strcmp(input, "") == 0)  || built_in(pipeline->cmd[0])) { //No se admiten built-in en pipelines
 			continue;
 		}
+		int res;
+		res = exec_pipe(pipeline);
 
-		//exec_cmd(cmd);
-		exec_pipe(pipeline);
-
-
+		if (res < 0) {
+			fprintf(stderr, "Error while executing the command");
+		}
+		clean(pipeline);
+		
 		if (redirection == 1) {
 			freopen("/dev/tty", "w", stdout);
 			freopen("/dev/tty", "r", stdin);
 			freopen("/dev/tty", "w", stderr);
 			redirection = 0;
 		}
+		background = 0;
 	}
 	exit(0);
 }
